8a4506eafededd667af90b3b8eb363a608d80406
Chris Kruining <chris@kruining.eu>
2019-01-08 19:52:33 +0100

    added the start of an implementation for plotting charts

---
+++ js/common/graphics/chart.js
@@ -0,0 +1,19 @@
+import * as Fyn from 'http://fyn-software.cpb/component/fyn.js';
+
+export default class Chart extends Fyn.Component
+{
+    static get properties()
+    {
+        return {
+            type: 'line',
+            interpolation: 'spline',
+        };
+    }
+    
+    ready()
+    {
+        const c = this.shadow.getElementById('c');
+        const ctx = c.getContext('2d');
+        
+    }
+}
