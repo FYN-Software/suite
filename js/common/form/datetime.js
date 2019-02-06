a24f74f992ab4cf1c2c51d41268a5afc8754bc98
Chris Kruining <chris@kruining.eu>
2019-02-06 20:08:42 +0100

    started implementation of datetime input

--- js/common/form/datetime.js
+++ js/common/form/datetime.js
@@ -1,6 +1,38 @@
-import Input from "./input.js";
+import * as Fyn from '../../../../component/fyn.js';
 
-export default class Datetime extends Input
+export default class Datetime extends Fyn.Component
 {
-    
+    static get properties()
+    {
+        return {
+            date: null,
+            label: '',
+        };
+    }
+
+    ready()
+    {
+        const box = this.shadow.querySelector('box');
+
+        this.on('fyn-common-form-input', {
+            click: Fyn.Event.debounce(1, () => {
+                const rect = this.getBoundingClientRect();
+
+                this.style.setProperty('--x', `${rect.x + rect.width / 2}px`);
+                this.style.setProperty('--y', `${rect.bottom}px`);
+
+                this.attributes.toggle('open');
+            }),
+        });
+
+        document.body.on({
+            click: (e) =>
+            {
+                if(e.path.includes(box) === false)
+                {
+                    this.removeAttribute('open');
+                }
+            },
+        });
+    }
 }
---
+++ js/common/misc/calander.js
@@ -0,0 +1,50 @@
+import * as Fyn from '../../../../component/fyn.js';
+
+export default class Calander extends Fyn.Component
+{
+    static get properties()
+    {
+        return {
+            year: 2019,
+            month: 2,
+            day: 6,
+            days: [],
+            firstDayOfMonth: [],
+        };
+    }
+
+    initialize()
+    {
+    }
+
+    ready()
+    {
+        {
+            const date = new Date();
+            this.year = date.getFullYear();
+            this.month = date.getMonth();
+        }
+
+        const date = new Date(this.year, this.month, 1);
+        this.firstDayOfMonth = date.getDay();
+        this.days = [];
+
+        while(date.getMonth() === this.month)
+        {
+            const day = new Date(date).getDate();
+
+            this.days.push(day);
+            date.setDate(date.getDate() + 1);
+        }
+
+        this.on('inner span', {
+            click: (e, t) => {
+                this.shadow
+                    .querySelectorAll('inner span[selected]')
+                    .forEach(el => el.removeAttribute('selected'));
+
+                t.setAttribute('selected', '');
+            },
+        });
+    }
+}
