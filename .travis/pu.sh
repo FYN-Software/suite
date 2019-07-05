9f87a95a6593c9c627916ecd4974cd00b3a54f21
Chris Kruining <c.kruining@fyn.nl>
2019-07-05 11:18:11 +0200

    next attempt

--- .travis/pu.sh
+++ .travis/pu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-jq -c '.version = "$newVersion"' --arg newVersion $1 ../package.json > temp.$$.json && mv temp.$$.json ../package.json
+jq -c '.version = "$newVersion"' --arg newVersion $1 package.json > temp.$$.json && mv temp.$$.json package.json
 
 echo "bumped package version to " $1
