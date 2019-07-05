6146162e23b37f3862f7ec3a7083fc0a74725642
Chris Kruining <c.kruining@fyn.nl>
2019-07-05 11:23:22 +0200

    debugging

--- .travis/pu.sh
+++ .travis/pu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-jq -c '.version = "$v"' --arg v $1 package.json > temp.$$.json && mv temp.$$.json package.json
+jq -c ".version = $v" --arg v $1 package.json > temp.$$.json && mv temp.$$.json package.json
 
-echo temp.$$.json
-echo package.json
+cat temp.$$.json
+cat package.json
 echo "bumped package version to " $1
