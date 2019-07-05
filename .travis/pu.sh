7be981e3da9408d8be8a4d5d537e0385c5470cf0
Chris Kruining <c.kruining@fyn.nl>
2019-07-05 11:27:12 +0200

    test

--- .travis/pu.sh
+++ .travis/pu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-jq -c ".version = $v" --arg v $1 package.json > temp.$$.json && mv temp.$$.json package.json
+jq -c '.version = "$v"' --arg v $1 package.json > temp.$$.json && mv temp.$$.json package.json
 
 cat temp.$$.json
 cat package.json
