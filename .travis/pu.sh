a3d1556f8e226877f8e9a8adc5321a37718a200b
Chris Kruining <c.kruining@fyn.nl>
2019-07-05 11:14:17 +0200

    test bumping

--- .travis/pu.sh
+++ .travis/pu.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "I am going to release " $1
+jq -c '.version = "$newVersion"' --arg newVersion $1 ../package.json > temp.$$.json && mv temp.$$.json ../package.json
+
+echo "bumped package version to " $1
