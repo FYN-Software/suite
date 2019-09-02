ac5d97d0678a8596275b159b83628684f053702a
Chris Kruining <c.kruining@fyn.nl>
2019-09-02 09:10:02 +0200

    added object fit as image property

--- js/common/graphics/image.js
+++ js/common/graphics/image.js
@@ -1,37 +1,47 @@
 import * as Fyn from '../../../../component/fyn.js';
 import * as Types from '../../../../data/types.js';
 
+export const Fit = Types.Enum.define({
+    auto: 'auto',
+    cover: 'cover',
+    contain: 'contain',
+});
+
 export default class Image extends Fyn.Component
 {
     static get properties()
     {
         return {
             src: Types.String.default(''),
+            fit: Fit.default(Fit.auto),
             loading: Types.Boolean.default(true),
         };
     }
 
     initialize()
     {
+        let img;
+
         this.observe({
             src: (o, n) => {
                 this.loading = true;
                 this.setAttribute('loading', '');
                 this.shadow.querySelectorAll('img').forEach(i => this.shadow.removeChild(i));
 
-                const img = document.createElement('img');
-                img.onload = () =>
-                {
+                img = document.createElement('img');
+                img.onload = () => {
                     this.shadow.appendChild(img);
                     this.loading = false;
                     this.removeAttribute('loading');
                 };
-                img.onerror = () =>
-                {
+                img.onerror = () => {
                     this.loading = false;
                     this.removeAttribute('loading');
                 };
                 img.src = String(this.src);
+            },
+            fit: (o, n) => {
+                img.style.setProperty('--fit', Fit.valueOf(n));
             },
         });
     }
