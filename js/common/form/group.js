3d63e12e9ab0c827d1cbaa7fc1e6b7ea25f159e5
Chris Kruining <c.kruining@fyn.nl>
2019-09-09 17:08:07 +0200

    set proper type

--- js/common/form/group.js
+++ js/common/form/group.js
@@ -1,9 +1,10 @@
 import * as Fyn from '../../../../component/fyn.js';
+import * as Types from '../../../../data/types.js';
 
 export default class Group extends Fyn.Component
 {
     static get properties()
     {
-        return { label: '' };
+        return { label: Types.String };
     }
 }
