385b5f48542bf8b0cedc96c146170e19439bbda1
Chris Kruining <c.kruining@fyn.nl>
2020-03-16 18:28:22 +0100

    added initial context menu implementation

--- js/common/navigation/context.js
+++ js/common/navigation/context.js
@@ -1,6 +1,11 @@
 import Component from '../../../../component/component.js';
 import * as Types from '../../../../data/types.js';
 
+export const Type = Types.Enum.define({
+    item: {},
+    separator: {},
+});
+
 export default class Context extends Component
 {
     static localName = 'fyn-common-navigation-context';
@@ -8,7 +13,50 @@ export default class Context extends Component
     static get properties()
     {
         return {
-            route: Types.String,
+            items: Types.List.type(Types.Object.define({
+                name: Types.String,
+                icon: Types.String,
+                action: Types.String,
+                type: Type,
+            })).default([
+                { name: 'copy', action: 'copy', icon: 'far.copy', type: Type.item },
+                { name: 'paste', action: 'paste', icon: 'paste', type: Type.item },
+                { name: 'delete', action: 'delete', icon: 'times', type: Type.item },
+                { type: Type.separator },
+            ]),
         };
+    }
+
+    async ready()
+    {
+        globalThis.on({ 'click|blur': () => this.close(), })
+    }
+
+    close()
+    {
+        this.removeAttribute('open');
+    }
+
+    static for(node, items)
+    {
+        const inst = new this({ items });
+        globalThis.document.body.appendChild(inst);
+
+        node.on({
+            options: {
+                passive: false,
+            },
+            contextmenu: e => {
+                e.preventDefault();
+                e.stopPropagation();
+
+                inst.style.setProperty('--x', `${e.x}px`);
+                inst.style.setProperty('--y', `${e.y}px`);
+
+                inst.setAttribute('open', '');
+            },
+        });
+
+        return inst;
     }
 }
