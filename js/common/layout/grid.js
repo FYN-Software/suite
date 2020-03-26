075d7146deea0f943c33a0d2b81f37ff7c2d34f9
Chris Kruining <c.kruining@fyn.nl>
2020-03-26 17:58:59 +0100

    started re-implementing docks

--- js/common/layout/grid.js
+++ js/common/layout/grid.js
@@ -5,4 +5,20 @@ export default class Grid extends Fyn.Component
 {
     static localName = 'fyn-common-layout-grid';
     static styles = [ 'fyn.suite.base' ];
+
+    static get properties()
+    {
+        return {
+            grid: Types.String,
+            cells: Types.List.type(Types.String),
+        }
+    }
+
+    async initialize()
+    {
+        this.observe({
+            grid: () => this.style.setProperty('--grid', this.grid),
+            cells: console.log,
+        });
+    }
 }
